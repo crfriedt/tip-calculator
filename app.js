034fa3f1cfef7ac404863c38ba4242e548cce9be
Cory Friedt <c.friedt89@gmail.com>
2020-11-01 14:10:27 -0800

    complete

--- app.js
+++ app.js
@@ -4,6 +4,38 @@ let totalPeople = document.getElementById("peopleInput");
 let tipPerPerson = document.getElementById("tipPerPerson");
 let totalPerPerson = document.getElementById("totalPerPerson");
 let calculateButton = document.getElementById("resultButton");
+let tipMinus = document.getElementById("tipMinus");
+let tipPlus = document.getElementById("tipPlus");
+let peopleMinus = document.getElementById("peopleMinus");
+let peoplePlus = document.getElementById("peoplePlus");
+
+tipMinus.addEventListener("click", function(){
+  var value = parseInt(document.getElementById('tipInput').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value--;
+  document.getElementById('tipInput').value = value;
+});
+
+tipPlus.addEventListener("click", function(){
+  var value = parseInt(document.getElementById('tipInput').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('tipInput').value = value;
+});
+
+peopleMinus.addEventListener("click", function(){
+  var value = parseInt(document.getElementById('peopleInput').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value--;
+  document.getElementById('peopleInput').value = value;
+});
+
+peoplePlus.addEventListener("click", function(){
+  var value = parseInt(document.getElementById('peopleInput').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('peopleInput').value = value;
+});
 
 calculateButton.addEventListener("click", function(){
   var billAmount = Number(billInput.value);
